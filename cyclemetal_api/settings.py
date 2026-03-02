d8dc38ebd906be16c45d8c3291269a00bf8908d2
amjavip <jsegural2300@alumno.ipn.mx>
2026-03-01 22:38:14 -0600

    fix: remove exposed secret key

--- cyclemetal_api/settings.py
+++ cyclemetal_api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zswcxsft+k!+_cgj(=7qrc5qn3-mla$7pd2#k+($6pbav7o@#b"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
